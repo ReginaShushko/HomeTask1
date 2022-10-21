1946763a7e22d9064885014a868f07e8882ec660
ReginaShushko <petrova.regina.mihajlovna@gamil.com>
2022-10-21 12:37:09 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int a, b, c, d, e;
+        a = 1;
+        b = a + 2;
+        c = b + 3;
+        d = c + 4;
+        e = d;
+
+        System.out.println("e =" +e);
+    }
+}
